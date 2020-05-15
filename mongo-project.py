2335e4fc94a7f153f3d5c0f029e241249a5b9f7a
Bill Davis <bill.davis@gmail.com>
2020-05-15 15:52:58 +0000

    Added changes to handled environment variables on GitPod

--- mongo-project.py
+++ mongo-project.py
@@ -1,7 +1,11 @@
 import pymongo
 import os
 
-MONGODB_URI = os.getenv("MONGO_URI")
+from os import path
+if path.exists("env.py"):
+    import env
+
+MONGODB_URI = os.environ.get("MONGO_URI")
 DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
 
